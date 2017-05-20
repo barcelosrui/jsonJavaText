1f7cf0217e896bbb44a9ee6d7163c9fa1330b79c
Rui Barcelos <Rui Barcelos@BarcelosD1-PC.home>
2017-05-20 15:19:08 +0100

    Teste JSON JAVA

--- TesteJson/src/testejson/TesteJson.java
+++ TesteJson/src/testejson/TesteJson.java
@@ -5,17 +5,69 @@
  */
 package testejson;
 
+import java.io.*;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+/**
 /**
  *
  * @author Rui Barcelos
  */
 public class TesteJson {
+    
+    public String geraJSON() throws IOException{
+        JSONObject obj = new JSONObject();
 
+        obj.put("name", "foo");
+        obj.put("num", new Integer(100));
+        obj.put("balance", new Double(1000.21));
+        obj.put("is_vip", new Boolean(true));
+
+        StringWriter out = new StringWriter();
+        obj.writeJSONString(out);
+      
+        String jsonText = out.toString();
+        return jsonText;
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         JSONParser parser = new JSONParser();
+      String s = "[0,{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}]";
+		
+      try{
+         Object obj = parser.parse(s);
+         JSONArray array = (JSONArray)obj;
+			
+         System.out.println("The 2nd element of array");
+         System.out.println(array.get(1));
+         System.out.println();
+
+         JSONObject obj2 = (JSONObject)array.get(1);
+         System.out.println("Field \"1\"");
+         System.out.println(obj2.get("1"));    
+
+         s = "{}";
+         obj = parser.parse(s);
+         System.out.println(obj);
+
+         s = "[5,]";
+         obj = parser.parse(s);
+         System.out.println(obj);
+
+         s = "[5,,2]";
+         obj = parser.parse(s);
+         System.out.println(obj);
+      }catch(ParseException pe){
+		
+         System.out.println("position: " + pe.getPosition());
+         System.out.println(pe);
+      }
     }
     
 }
